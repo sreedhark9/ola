fbe6bb124523200105072b7c048ba211ac8c084f
sreedhark9 <scorpiosreedhar@gmail.com>
2021-08-08 15:01:24 +0530

    added first file

---
+++ DButiles.java
@@ -0,0 +1,7 @@
+Hello guys..
+echo "please tell me your name"
+read
+echo "the name which you have entered is:"
+please enter your name
+Sreedhar
+
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+helo guys
+wer r u 
+right 
+note {
+
+stage("checkoutcode") {
+git link taken 
+}
